0619903d5f71f185182658b31693ac495e098aa3
Aaditya-web <76609534+Aaditya-web@users.noreply.github.com>
2022-06-14 14:19:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,25 @@
+leftWristx=0;
+rightWristx=0;
+difference=0;
 
 function preload(){
 
 }
 function setup() {
-    canvas = createCanvas(400, 300);
-    canvas.center();
-    canvas.position(800,200);
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
 
     video = createCapture(VIDEO);
-    video.hide();
-    video.size(400,300);
+    video.size(550, 550);
 
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
 }
 
 function draw() {
-    image(video, 0, 0, 600, 500);
+    image(video, 550, 550, 550, 550);
+    text("Aaditya", 560, 150);
+    textSize(difference);
 }
 
 function modelLoaded() {
@@ -27,4 +30,9 @@ function modelLoaded() {
 function gotPoses(results) {
     if (results.length > 0) 
         console.log(results);
+        leftWristx = results[0].pose.leftWrist.x;
+        rightWristx = results[0].pose.rightWrist.x;
+        
+        difference= floor(leftWristx-rightWristx);
+        document.getElementById("font-size").innerHTML= difference;
     };
